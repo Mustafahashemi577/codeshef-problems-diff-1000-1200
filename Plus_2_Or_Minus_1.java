a2c3989eeb97b11e5faf66e21fea5884193166be
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2024-04-21 12:41:21 +0430

    Add files via upload
    https://www.codechef.com/problems/PLUS2MINUS1
    Plus_2_Or_Minus_1 problem with difficulty of 1098 code in Java

---
+++ Plus_2_Or_Minus_1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Plus_2_Or_Minus_1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner (System.in);
+		int t = scan.nextInt();
+		while(t-->0){
+		    int y = scan.nextInt();
+		    if(y==0)
+		    System.out.println(1);
+		    else
+		    System.out.println(y*3);
+		    
+		}
+
+	}
+}
